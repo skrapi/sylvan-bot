ea703f62ae14af1b0a04d513c353d6a7d41e697b
Sylvan Smit <sjsmit@amazon.com>
2023-10-28 14:26:34 +0200

    fixing because the tutorial does not explain

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,127 @@
-use serenity::model::prelude::InteractionResponseType;
-use serenity::model::prelude::Interaction;
-use serenity::model::prelude::GuildId;
-use anyhow::anyhow;
-use serenity::async_trait;
-use serenity::model::channel::Message;
+mod weather;
+
+use anyhow::Context as _;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::command::CommandOptionType;
 use serenity::prelude::*;
+use serenity::{async_trait, model::prelude::GuildId};
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
+use weather::get_forecast;
 
-struct Bot;
+struct Bot {
+    weather_api_key: String,
+    client: reqwest::Client,
+    discord_guild_id: GuildId,
+}
+
+#[shuttle_service::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_service::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
+    let weather_api_key = secret_store
+        .get("WEATHER_API_KEY")
+        .context("'WEATHER_API_KEY' was not found")?;
+
+    let discord_guild_id = secret_store
+        .get("DISCORD_GUILD_ID")
+        .context("'DISCORD_GUILD_ID' was not found")?;
+
+    let client = get_client(
+        &discord_token,
+        &weather_api_key,
+        discord_guild_id.parse().unwrap(),
+    )
+    .await;
+
+    Ok(client)
+}
+
+pub async fn get_client(
+    discord_token: &str,
+    weather_api_key: &str,
+    discord_guild_id: u64,
+) -> Client {
+    // Set gateway intents, which decides what events the bot will be notified about.
+    // Here we don't need any intents so empty
+    let intents = GatewayIntents::empty();
+
+    Client::builder(discord_token, intents)
+        .event_handler(Bot {
+            weather_api_key: weather_api_key.to_owned(),
+            client: reqwest::Client::new(),
+            discord_guild_id: GuildId(discord_guild_id),
+        })
+        .await
+        .expect("Err creating client")
+}
+
+const OPTION_NAME: &str = "place";
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
     async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
 
-        let guild_id = GuildId(1167504487448059945);
+        let commands =
+            GuildId::set_application_commands(&self.discord_guild_id, &ctx.http, |commands| {
+                commands
+                    .create_application_command(|command| {
+                        command.name("hello").description("Say hello")
+                    })
+                    .create_application_command(|command| {
+                        command
+                            .name("weather")
+                            .description("Display the weather")
+                            .create_option(|option| {
+                                option
+                                    .name(OPTION_NAME)
+                                    .description("City to lookup forecast")
+                                    .kind(CommandOptionType::String)
+                                    .required(true)
+                            })
+                    })
+            })
+            .await
+            .unwrap();
 
-        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
-            commands.create_application_command(|command| { command.name("hello").description("Say hello") })
-        }).await.unwrap();
-
-        info!("{:#?}", commands);
+        info!("Registered commands: {:#?}", commands);
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             let response_content = match command.data.name.as_str() {
-                "hello" => "hello".to_owned(),
-                command => unreachable!("Unknown command: {}", command),
+                "hello" => "hello".to_string(),
+                "weather" => {
+                    let argument = command
+                        .data
+                        .options
+                        .iter()
+                        .find(|opt| opt.name == OPTION_NAME)
+                        .cloned();
+
+                    let value = argument.unwrap().value.unwrap();
+                    let place = value.as_str().unwrap();
+
+                    match get_forecast(place, &self.weather_api_key, &self.client).await {
+                        Ok((location, forecast)) => {
+                            format!("Forecast: {} in {}", forecast.headline.overview, location)
+                        }
+                        Err(err) => {
+                            format!("Err: {}", err)
+                        }
+                    }
+                }
+                command => {
+                    error!("Interaction made with unknown command: {}", command);
+                    format!("Unknown command: {}", command)
+                }
             };
 
             let create_interaction_response =
@@ -48,30 +132,8 @@ impl EventHandler for Bot {
                 });
 
             if let Err(why) = create_interaction_response.await {
-                eprintln!("Cannot respond to slash command: {}", why);
+                error!("Cannot respond to slash command: {}", why);
             }
         }
     }
-}
-
-#[shuttle_runtime::main]
-async fn serenity(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
-
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
-        .expect("Err creating client");
-
-    Ok(client.into())
 }
